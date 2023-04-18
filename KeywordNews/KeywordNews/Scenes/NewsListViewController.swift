85074b60579bbc8cd679ec1831b20a28e44a2564
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-04-18 18:09:28 +0900

    feat: NewsListViewController UI 구현 및 레이아웃 구성

--- KeywordNews/KeywordNews/Scenes/NewsListViewController.swift
+++ KeywordNews/KeywordNews/Scenes/NewsListViewController.swift
@@ -5,8 +5,51 @@ import UIKit
 import SnapKit
 
 final class NewsListViewController: UIViewController {
+ 
+    private var presenter: NewsListPresenter?
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.register(NewsListCell.self,
+                           forCellReuseIdentifier: NewsListCell.identifier)
+        
+        return tableView
+    }()
+    
+    private let refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+
+        return refreshControl
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupPresenter()
+        setupNavigationBar()
+        setupView()
+    }
+    
+    private func setupPresenter() {
+        presenter = NewsListPresenter(viewController: self)
+        tableView.delegate = presenter
+        tableView.dataSource = presenter
+        tableView.refreshControl = refreshControl
+    }
+    
+    private func setupView() {
+        view.addSubview(tableView)
+
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = "NEWS"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
     }
 }
